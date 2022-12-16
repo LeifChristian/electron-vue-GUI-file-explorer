2ce6e932e64a9e2b2daac568d6ad7144a52579a9
lchristian@unitedlocating.com-at-530555730387 <mtmusicandart@gmail.com>
2022-12-16 05:57:07 -0700

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "../store/counter";
+defineProps<{ msg: string }>();
+
+const { count } = storeToRefs(useCounterStore());
+</script>
+
+<template>
+ hi
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+.routerlink {
+  margin: 0 10px;
+}
+</style>
